704710b774f4cde165323e81af98655432ec5b6a
VandersonCWB <vandersonja@hotmail.com>
2018-08-04 19:14:51 -0300

    Ajustes

--- algamoney-api/src/main/java/com/algaworks/AlgamoneyApiApplication.java
+++ algamoney-api/src/main/java/com/algaworks/AlgamoneyApiApplication.java
@@ -3,6 +3,11 @@ package com.algaworks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe responsavel por rodar a aplicação Spring Boot
+ * @author Vanderson J de Araújo
+ * @since 08/2018
+ */
 @SpringBootApplication
 public class AlgamoneyApiApplication {
 
